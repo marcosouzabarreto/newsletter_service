3651391e7af30dbf78e7a2d96c6d17af6eb87348
Marco Barreto <marcosouzabarreto@gmail.com>
2024-11-16 17:12:40 -0300

    feat: Improved telemetry code reusability

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,24 @@
 mod routes;
 mod startup;
-use newsletter_service::configuration::get_configuration;
+use newsletter_service::{
+    configuration::get_configuration,
+    telemetry::{get_subscriber, init_subscriber},
+};
 use sqlx::PgPool;
 use startup::run;
 use std::net::TcpListener;
-use tracing::dispatcher::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("newsletter_service".to_string(), "info".to_string());
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to get configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to read configuration");
+
     let address = format!("0.0.0.0:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Error to bind random port");
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("newsletter_service".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber.into()).expect("Failed to set subscriber");
     run(listener, connection_pool)?.await
 }
