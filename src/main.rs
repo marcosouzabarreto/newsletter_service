3511f50401c522d44ba57e13bf6d9abd4c2721f4
Marco Barreto <marcosouzabarreto@gmail.com>
2024-11-16 16:13:30 -0300

    feat: Improve logs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod routes;
 mod startup;
-use env_logger::Env;
 use newsletter_service::configuration::get_configuration;
 use sqlx::PgPool;
 use startup::run;
 use std::net::TcpListener;
+use tracing::dispatcher::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -14,6 +16,12 @@ async fn main() -> Result<(), std::io::Error> {
         .expect("Failed to read configuration");
     let address = format!("0.0.0.0:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Error to bind random port");
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("newsletter_service".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber.into()).expect("Failed to set subscriber");
     run(listener, connection_pool)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -31,9 +31,9 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     );
     let result = sqlx::query!(
         r#"
-    INSERT INTO subscriptions(id, email, name, subscribed_at)
-    VALUES ($1, $2, $3, $4)
-    "#,
+            INSERT INTO subscriptions(id, email, name, subscribed_at)
+            VALUES ($1, $2, $3, $4)
+        "#,
         Uuid::new_v4(),
         form.email,
         form.name,
